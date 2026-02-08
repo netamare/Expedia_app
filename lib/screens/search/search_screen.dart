56499e91875a8bdcd766001e34049a92907cc514
Nesti Amare <nestiamare64@gmail.com>
2026-02-08 22:13:47 +0300

    update search screen

--- lib/screens/search/search_screen.dart
+++ lib/screens/search/search_screen.dart
@@ -1,47 +1,10 @@
 import 'package:flutter/material.dart';
-import 'hotel_search_screen.dart';
-import 'flight_search_screen.dart';
+import '../../main.dart';
 import 'car_search_screen.dart';
 
-class SearchScreen extends StatefulWidget {
-  @override
-  _SearchScreenState createState() => _SearchScreenState();
-}
-
-class _SearchScreenState extends State<SearchScreen> with SingleTickerProviderStateMixin {
-  late final TabController _tabController;
-  final tabs = ['Hotels', 'Flights', 'Cars'];
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(length: tabs.length, vsync: this);
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
-  }
-
+class SearchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Search'),
-        bottom: TabBar(
-          controller: _tabController,
-          tabs: tabs.map((t) => Tab(text: t)).toList(),
-        ),
-      ),
-      body: TabBarView(
-        controller: _tabController,
-        children: [
-          HotelSearchScreen(),
-          FlightSearchScreen(),
-          CarSearchScreen(),
-        ],
-      ),
-    );
+    return CarSearchScreen(); // or whatever layout you want
   }
 }
